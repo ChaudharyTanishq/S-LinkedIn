36ae84c047b92e11c0f08521d2d02137851001ee
smartypants042 <tanishqchaudhary511@gmail.com>
2021-01-10 15:31:19 +0530

    Adding Database connection for MongoDb. INSECURE.

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+// allows to create routes
+const app = express()
+
+// basic requirements
+app.use(express.json())
+app.use(cors())
+
+// adding user and boss routers
+const userRouter = require('./routes/user')
+// const bossRouter = require('./routes/boss')
+
+// specifying what routes they will take
+app.use('/user', userRouter)
+// app.use('/boss', bossRouter)
+
+
+// database
+mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0.x9hsu.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => { console.log('database connected successfully!')}
+)
+
+
+// port on which backend is hosted
+const PORT = 5000
+
+// listening on port
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}!`)
+})
