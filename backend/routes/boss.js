3365475e002d054b5b81577e8d34930835f6a432
smartypants042 <tanishqchaudhary511@gmail.com>
2021-01-13 09:25:31 +0530

    Updating new routes for User and Boss functionalities

--- backend/routes/boss.js
+++ backend/routes/boss.js
@@ -3,7 +3,12 @@ const boss = require('../controllers/boss')
 const { bossAuth, userAuth } = require('../controllers/verifyToken')
 
 const router = express.Router()
-router.get('/', bossAuth, boss.getProfile)
-router.post('/create', bossAuth, boss.createJob)
+router.get('/', boss.getProfile)
+router.get('/myJobs', boss.getMyJobs)
+router.post('/create', boss.createJob)
+router.get('/:jobId', boss.showJob)
+router.post('/:jobId', boss.updateApplicationsJob)
+router.delete('/:jobId', boss.deleteJob)
+router.patch('/:jobId', boss.updateJob)
 
 module.exports = router
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const user = require('../controllers/user')
+const JobDesc = require('../models/JobDesc')
 const { bossAuth, userAuth } = require('../controllers/verifyToken')
 
 const router = express.Router()
-router.get('/', userAuth, user.getDashboard)
-router.get('/profile', userAuth, user.getProfile)
+router.get('/', user.getDashboard)
+router.get('/profile', user.getProfile)
+router.get('/applications', user.getApplications)
+router.get('/:jobId', user.showJob)
+router.post('/:jobId', user.applyJob)
 
 module.exports = router
