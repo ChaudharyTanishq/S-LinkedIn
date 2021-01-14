10f5ea83809e51aa9379ad1fb4e76fdd5feb769b
smartypants042 <tanishqchaudhary511@gmail.com>
2021-01-14 19:31:12 +0530

    Add: Frontend: src: components: Templates!

---
+++ frontend/src/components/Boss/boss.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Boss(props) {
+	return (
+		<div className="Boss">
+			<h1>Boss</h1>
+		</div>
+	)
+}
+
+export default Boss
---
+++ frontend/src/components/User/dashboard.js
@@ -0,0 +1,24 @@
+// const [isLoading, data] = useApiGet('/user')
+	
+// let content 
+// if (isLoading) content = 'loading ...'
+// else if (!data) content = 'error fetching data'
+// else {
+//     content = 'work in progress'
+//     for (let i in data){
+//         console.log(data[i])
+//         console.log("=================================")
+//     }
+// }
+
+import React from 'react' 
+
+function Dashboard(props) {
+	return (
+		<div className="Dashboard">
+			<h1>Dashboard</h1>
+		</div>
+	)
+}
+
+export default Dashboard;
