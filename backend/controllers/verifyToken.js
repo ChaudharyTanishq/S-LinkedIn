f65937882f3b373f057ed3d5e9399fba6265dac9
smartypants042 <tanishqchaudhary511@gmail.com>
2021-01-14 19:22:36 +0530

    Adding function to get the current person: default/user/boss

--- backend/controllers/verifyToken.js
+++ backend/controllers/verifyToken.js
@@ -1,6 +1,23 @@
 const jwt = require('jsonwebtoken')
 const People = require('../models/People')
 
+// internal function needed to determine who is the one signed in
+async function getWho (req, res){
+    const token = req.header('auth-token')
+    // console.log('auth-token-backend', token)
+    if (!token) return res.status(401).send("not logged in!")
+
+    try {
+        const verified = jwt.verify(token, "TOKEN_SECRET")
+        req.user = verified
+        const person = await People.findOne({_id: req.user._id})
+        if(person.isBoss) return res.status(200).send(true)
+        else return res.status(200).send(false) 
+    } catch(error) {
+        return res.status(400).send("invalid token!")
+    }
+}
+
 async function userAuth (req, res, next){
     const token = req.header('auth-token')
     if (!token) return res.status(401).send("access denied. login!")
@@ -33,4 +50,4 @@ async function bossAuth (req, res, next){
     }
 }
 
-module.exports = { bossAuth, userAuth }
+module.exports = { bossAuth, userAuth, getWho }
