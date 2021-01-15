c7e06e322ac6ced15d40a1526bfaf1d286f71e17
smartypants042 <tanishqchaudhary511@gmail.com>
2021-01-15 15:52:20 +0530

    Updating: App with context: Frontend: src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,69 @@
-import React from 'react' 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { generateApi, useApiGet } from "./components/Utility/api";
 
-import NavBar from './components/Utility/navbar'
+import NavBar from "./components/Utility/navbar";
 import User from "./components/User/user";
 import Boss from "./components/Boss/boss";
 import Home from "./components/Utility/home";
-import Login from './components/Utility/login';
-
+import Login from "./components/Utility/login";
+import Dashboard from "./components/User/dashboard";
+import { JobDetails } from "./components/User/jobDetails";
+import Logout from "./components/Utility/logout";
+import { UserContext } from "./components/Utility/userContext";
 
 function App(props) {
-	const person = {
-		'default': ['login', 'register'],
-		'user': ['dashboard', 'profile', 'applications'],
-		'boss': ['create', 'view', 'accepted']
-	}
+  const person = {
+    default: ["login", "register"],
+    user: ["dashboard", "profile", "applications"],
+    boss: ["create", "myJobs", "accepted"],
+  };
 
-	
-	// REMOVE THE HARDCODING HERE,
-	// WITH TOKEN AND IDENTIFYING USER
-	let who = 'default'
-	const authToken = localStorage.getItem('person')
-	// console.log(authToken)
-	const api = generateApi(authToken)
-	const [isLoading, data] = useApiGet(api, '/default/who')
-	console.log(data)
-	if(authToken){
-		if(!isLoading && data === null) who = 'default'
-		else if (!isLoading && data === true) who = 'boss'
-		else if (!isLoading && data === false) who = 'user'
-	}
+  const [authToken, setauthToken] = useState('');
+  // REMOVE THE HARDCODING HERE,
+  // WITH authToken AND IDENTIFYING USER
+  let who = "default";
+  let personName = "";
+  const api = generateApi(authToken);
+  const [isLoading, data, errorData] = useApiGet(api, "/default/who", [who, authToken]);
+  if (authToken) {
+    if (!isLoading && data === null) who = "default";
+    else if (!isLoading && data.isBoss === true) {
+      who = "boss";
+      personName = data.name;
+    } else if (!isLoading && data.isBoss === false) {
+      who = "user";
+      personName = data.name;
+    }
+  }
+  // setting the current person
+  const currentPerson = person[who];
 
-	const currentPerson = person[who]
-
-	return (
-		<Router>
-			<div className="App">
-				<NavBar person={currentPerson} who={who}/>
-				<h1>S-LinkedIn</h1>
-				<Switch>
-					<Route path="/default" exact component={Home}/>
-					<Route path="/default/login" exact component={Login}/>
-					<Route path="/user" component={User}/>
-					{/* <Route path="/user/profile" component={User}/>
-					<Route path="/user/dashboard" component={User}/>
-					<Route path="/user/appplications" component={User}/> */}
-					<Route path="/boss" component={Boss}/>
-					{/* <Route path="/boss/view" component={Boss}/>
-					<Route path="/boss/accepted" component={Boss}/> */}
-				</Switch>
-			</div>
-		</Router>
-	)
+  return (
+    <Router>
+      <div className="App">
+        <NavBar person={currentPerson} who={who} personName={personName} />
+        <h1>S-LinkedIn</h1>
+        <Switch>
+          <UserContext.Provider value={{ authToken, setauthToken }}>
+            <Route path="/default" exact component={Home} />
+            <Route path="/default/login" exact component={Login} />
+            <Route path="/default/logout" exact component={Logout} />
+            <Route path="/user" exact component={User} />
+            <Route path="/user/dashboard" exact component={Dashboard} />
+            <Route path="/user/:jobId" component={JobDetails} />
+            {/* <Route path="/user/profile" component={User}/> */}
+            {/* <Route path="/user/appplications" component={User}/> */}
+            <Route path="/boss" exact component={Boss} />
+            {/* <Route path="/boss/myJobs" exact component={Boss} /> */}
+            {/* <Route path="/boss/view" component={Boss}/> */}
+            {/* <Route path="/boss/accepted" component={Boss}/> */}
+          </UserContext.Provider>
+        </Switch>
+      </div>
+      {/* {errorData !== ""? errorData: ""} */}
+    </Router>
+  );
 }
 
 export default App;
